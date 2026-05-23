9bfc8a82d91fbbef07c3b700837af2db23d5da1c
Dmytro Herman <287163259+dmytrohrm@users.noreply.github.com>
2026-05-23 11:22:28 +0300

    Fix car form and delete confirmation

--- taxi/views.py
+++ taxi/views.py
@@ -82,6 +82,7 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
+    template_name = "taxi/car_confirm_delete.html"
     success_url = reverse_lazy("taxi:car-list")
 
 
